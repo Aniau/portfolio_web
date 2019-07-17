215167411ce7230031c5856690af0f3881a763a0
Anna Ulanecka <ania.ulanecka@gmail.com>
2019-07-17 21:55:33 +0200

    Add project to github.

--- Google_search/booksearch.js
+++ Google_search/booksearch.js
@@ -1,67 +1,128 @@
-var a=0; // startowa wartość indeksu na 0
-var b=12; // ilość załadowanych wyników
+var a=0; 
+var b=40; 
 var licznik2=0;
 
-function bookSearch() // deklaracja funkcji
+function bookSearch() 
 {
-	var szukaj = document.getElementById('search').value // pobranie wartości z pola tekstowego i wsadzenie do zmiennej szukaj
-	var szukajJezyk = document.getElementById('lang').value // 
+	var szukaj = document.getElementById('search').value
 	var autor = document.getElementById('author').value
 	var dataP = document.getElementById('date').value
-	var content = document.getElementById('content'); // załadowanie bieżącej zawartości do zmiennej
-	var error = document.getElementById('p'); // załadowanie bieżącej zawartości do zmiennej
-	var next="php&startIndex="+a+"&maxResults="+b; // utworzenie kolejnej iteracji wyników
-	a+=12; //zmiana startowej wartości indeksu o kolejne 12 wyników
+	var content = document.getElementById('content');
+	var error = document.getElementById('p'); 
+		var next="php&startIndex="+a+"&maxResults="+b; 
+		
+		a+=40; 
 
 	$.ajax({
-		url: "https://www.googleapis.com/books/v1/volumes?q=" +szukaj+"&"+next,dataType: "json",success: function(data)
-		{ // konkatenacja wysyłanego zapytania
-				for(i=0;i<data.items.length;i++)
-				{ // załadowanie odpowiedniej ilości wyników w pętli
-							var tytul = data.items[i].volumeInfo.title; // pobranie do zmiennej opis, opisu ksiazki w kazdej iteracji
-							var opis = data.items[i].volumeInfo.description; // pobranie do zmiennej opis, opisu ksiazki w kazdej iteracji
+		url: "https://www.googleapis.com/books/v1/volumes?q=" +szukaj+"&"+next,dataType: "json",success: function(data){ 
+			
+				for(i=0;i<data.items.length;i++){ 
+							var tytul = data.items[i].volumeInfo.title;
+							var opis = data.items[i].volumeInfo.description; 
 							var autorzy = data.items[i].volumeInfo.authors;
 							var datam = data.items[i].volumeInfo.publishedDate;
 							var jezyk = data.items[i].volumeInfo.language;
 							var x = autorzy;
+							licznik2++;
+							if((document.getElementById('lang').value)!=""){
+										var szjezyk=document.getElementById('lang').value;
+										var c=(jezyk===szjezyk);
+										console.log("Pozycja: "+licznik2+". Czy szukany jezyk: "+szjezyk+" jest taki jak książki "+jezyk+" wynik: "+c);
+										
+											if(c){
+																	if (tytul == undefined){ 
+																				tytul = "Missing title"; 
+																	}
+																	
+																	else{
+																				var tytul1;
+																				tytul1 = tytul.slice(0,60);
+																				tytul = tytul1 + "..."; 
+																				tytul=(tytul.toUpperCase());
+																	}
+																	
+																	if (opis == undefined){ 
+																		opis = "Missing description"; 
+																	}
+																	
+																	else{
+																			var opis1;
+																			opis1 = opis.slice(0,150);
+																			opis = opis1 + "..."; 
+																	}
+															
+																	if (autorzy == undefined){ 
+																			autorzy = "Missing authors"; 
+																	}
+																	
+															const ks = document.createElement('div');
+															ks.setAttribute("class","pozycja");
+															const h4 = document.createElement('h4');
+															const h5 = document.createElement('h5');
+															const h6 = document.createElement('h6');
+															const h3 = document.createElement('h3');
+															const img = document.createElement('img');				
+															h4.innerHTML = tytul;
+															h5.innerHTML = autorzy;
+															h6.innerHTML = opis;
+															h3.innerHTML = datam;
+															img.setAttribute('src', data.items[i].volumeInfo.imageLinks.thumbnail);
 
-							if((document.getElementById('lang').value)!="")
-							{
-							var szjezyk=document.getElementById('lang').value;
-							var c=(jezyk===szjezyk);
-							console.log("Pozycja: "+licznik2+". Czy szukany jezyk: "+szjezyk+" jest taki jak książki "+jezyk+" wynik: "+c);
-							
-							if(c)
-							{
-									if (tytul == undefined){ // sprawdzenie czy tytul poprawnie się pobrał
-								tytul = "Missing title"; // jeśli nie znaleziono tytulu, wyswietla komunikat
+
+															ks.append(h4);
+															ks.append(h5);
+															ks.append(h6);
+															ks.append(h3);
+															ks.append(img);
+
+															content.append(ks);
+															console.log("dodanie");
+													}
+													else{
+														continue;
+													}
 							}
-							else
-							{
+							
+							if((document.getElementById('date').value)!=""){
+									var dataP = document.getElementById('date').value;
+									var dat = dataP.slice(0,4);
+									var dan = datam.slice(0,4);
+									var d = (dan == dat);
+									console.log(licznik2+". Czy "+dat+" jest równa "+dan+" ? "+d);
+							}
+
+							if((document.getElementById('author').value)!=""){
+									var autor=document.getElementById('author').value;
+									var sx=x.toString();
+									var sautor=autor.toString();
+									var n= sx.indexOf(sautor);
+									console.log("Pozycja: "+licznik+". Czy w "+x+" zawiera się wyraz: "+autor+" wynik: "+n);
+							}
+							
+							if( n != (-1)){ 
+							if (tytul == undefined){ 
+								tytul = "Missing title"; 
+							}
+							else{
 								var tytul1;
-								tytul1 = tytul.slice(0,60); // uciecie tytulu do 150 znaków
-								tytul = tytul1 + "..."; // dodanie "..." do tytulu
+								tytul1 = tytul.slice(0,60); 
+								tytul = tytul1 + "..."; 
 								tytul=(tytul.toUpperCase());
 							}
-							if (opis == undefined){ // sprawdzenie czy opis poprawnie się pobrał
-								opis = "Missing description"; // jeśli nie znaleziono opisu, wyswietla komunikat
+							if (opis == undefined){ 
+								opis = "Missing description";
 							}
-							else
-							{
+							else{
 								var opis1;
-								opis1 = opis.slice(0,150); // uciecie opisu do 150 znaków
-								opis = opis1 + "..."; // dodanie "..." do opisu
+								opis1 = opis.slice(0,150); 
+								opis = opis1 + "..."; 
 							}
 							
-								if (autorzy == undefined){ // sprawdzenie czy opis poprawnie się pobrał
-								autorzy = "Missing authors"; // jeśli nie znaleziono opisu, wyswietla komunikat
+								if (autorzy == undefined){
+								autorzy = "Missing authors"; 
 							}
-								
-								
-							// wsadzenie odpowiednich elementow do tagów html
 							const ks = document.createElement('div');
 							ks.setAttribute("class","pozycja");
-							//utworzenie klasy bloku "pozycja" dla pojedynczego wyniku - dla css
 							const h4 = document.createElement('h4');
 							const h5 = document.createElement('h5');
 							const h6 = document.createElement('h6');
@@ -82,172 +143,64 @@ function bookSearch() // deklaracja funkcji
 
 							content.append(ks);
 							console.log("dodanie");
-									}
-							else
-							{
-								continue;
 							}
+							else{ 
+							continue;
 							}
-							
-							if((document.getElementById('date').value)!="")
-							{
-								console.log(datam);
-							}
-
-							if((document.getElementById('author').value)!="")
-							{
-								var autor=document.getElementById('author').value;
-								var sx=x.toString(); // sx - pobrana nazwa autora
-								var sautor=autor.toString(); // sautor - wpisana nazwa autora
-								
-								
-								var aaa = sx.toUpperCase();
-								sx=aaa;
-								var bbb = sautor.toUpperCase();
-								sautor=bbb;
-								
-								var n=  sx.indexOf(sautor); // sprawdzenie czy wpisana nazwa wystepuje w pobranej
-								
-								
-								console.log("Pozycja: "+licznik+". Czy w "+sx+" zawiera się wyraz: "+sautor+" wynik: "+n);
-								console.log(sautor + n);
-							}
-							
-							if( n != (-1))
-							{ 
-								if (tytul == undefined)
-								{
-									// sprawdzenie czy tytul poprawnie się pobrał
-									tytul = "Missing title"; // jeśli nie znaleziono tytulu, wyswietla komunikat
-								}
-								else
-								{
-									var tytul1;
-									tytul1 = tytul.slice(0,60); // uciecie tytulu do 150 znaków
-									tytul = tytul1 + "..."; // dodanie "..." do tytulu
-									tytul=(tytul.toUpperCase());
-								}
-								if (opis == undefined)
-								{ // sprawdzenie czy opis poprawnie się pobrał
-									opis = "Missing description"; // jeśli nie znaleziono opisu, wyswietla komunikat
-								}
-								else
-								{
-									var opis1;
-									opis1 = opis.slice(0,150); // uciecie opisu do 150 znaków
-									opis = opis1 + "..."; // dodanie "..." do opisu
-								}
-								
-								if (autorzy == undefined)
-								{ // sprawdzenie czy opis poprawnie się pobrał
-									autorzy = "Missing authors"; // jeśli nie znaleziono opisu, wyswietla komunikat
-								}
-							
-							
-								// wsadzenie odpowiednich elementow do tagów html
-								const ks = document.createElement('div');
-								ks.setAttribute("class","pozycja");
-								//utworzenie klasy bloku "pozycja" dla pojedynczego wyniku - dla css
-								const h4 = document.createElement('h4');
-								const h5 = document.createElement('h5');
-								const h6 = document.createElement('h6');
-								const h3 = document.createElement('h3');
-								const img = document.createElement('img');				
-								h4.innerHTML = tytul;
-								h5.innerHTML = autorzy;
-								h6.innerHTML = opis;
-								h3.innerHTML = datam;
-								img.setAttribute('src', data.items[i].volumeInfo.imageLinks.thumbnail);
-
-
-								ks.append(h4);
-								ks.append(h5);
-								ks.append(h6);
-								ks.append(h3);
-								ks.append(img);
-
-								content.append(ks);
-							}
-							else
-							{ 
-								continue;
-							}
-				}
+	}
 		},
 		type: 'GET'	
 	});	
 }
-document.getElementById('button').addEventListener('click', bookSearch, false) // wywołanie funkcji przez przycisk
+document.getElementById('button').addEventListener('click', bookSearch, false) 
 
-// wywolywanie funkcji pobierajacej wyniki poprzez scrollowanie strony
 var licznik=1;
 var max1=0;
-onwheel = function()
-{
-	licznik=window.pageYOffset;
-
-	if(licznik>max1)
-	{
-		max1=licznik;
+onwheel = function(){
+licznik=window.pageYOffset;
+if(licznik>max1){
+	max1=licznik;
 	}
-	if(licznik===max1)
-	{
-		bookSearch();
+if(licznik===max1){
+	bookSearch();
 	}
 }
 
-// dezaktywowanie i aktywowanie przycisku 'search', gdy pola sa puste lub uzupelnione
-	$('#search').keyup(function() 
-	{
-		if ($('#search').val() == "") 
-		{
+	$('#search').keyup(function() {
+		if ($('#search').val() == "") {
 			$('#button').disabled = true;
 			$('#searchSpan').addClass('gray').removeClass('white');
-		} 
-		else 
-		{
+		} else {
 			$('#button').disabled = false;
 			$('#searchSpan').addClass('white').removeClass('gray');
 		}
 	});
 	
-	$('#lang').keyup(function() 
-	{
-		if ($('#lang').val() == "") 
-		{
+	$('#lang').keyup(function() {
+		if ($('#lang').val() == "") {
 			$('#button').disabled = true;
 			$('#searchSpan').addClass('gray').removeClass('white');
-		} 
-		else 
-		{
+		} else {
 			$('#button').disabled = false;
 			$('#searchSpan').addClass('white').removeClass('gray');
 		}
 	});
 	
-	$('#date').keyup(function() 
-	{
-		if ($('#date').val() == "") 
-		{
+	$('#date').keyup(function() {
+		if ($('#date').val() == "") {
 			$('#button').disabled = true;
 			$('#searchSpan').addClass('gray').removeClass('white');
-		} 
-		else 
-		{
+		} else {
 			$('#button').disabled = false;
 			$('#searchSpan').addClass('white').removeClass('gray');
 		}
 	});
 	
-	$('#author').keyup(function() 
-	{
-		if ($('#author').val() == "") 
-		{
+	$('#author').keyup(function() {
+		if ($('#author').val() == "") {
 			$('#button').disabled = true;
 			$('#searchSpan').addClass('gray').removeClass('white');
-		} 
-		else
-		{
+		} else {
 			$('#button').disabled = false;
 			$('#searchSpan').addClass('white').removeClass('gray');
 		}
